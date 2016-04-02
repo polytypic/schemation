46903f5d0655645d20159ba2031c800e89b5a892
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-04-02 12:30:55 +0300

    Updated deps.  Using eval in tests.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
 import {and, any, number, optional, or, lazy} from "../src/schemation"
 import {tryMatch, validate} from "../src/schemation"
 
+const branch = {lhs: optional(lazy(() => branch)),
+                key: number,
+                rhs: optional(lazy(() => branch))}
+const tree = or(null, branch)
+
 const isntType = type => json => typeof json !== type
 
 const assertEq = expected => actual => {
@@ -18,17 +23,21 @@ const show = x => {
   }
 }
 
-const matches = (schemaString, schema, ...jsons) => jsons.forEach(
-  json => it(`${schemaString} matches ${show(json)}`, () => {
-    if (json !== validate(schema)(json))
+const E = schema =>
+  eval(`(and, any, number, optional, or, lazy, tree) => (${schema})`)(
+         and, any, number, optional, or, lazy, tree)
+
+const matches = (schema, ...jsons) => jsons.forEach(
+  json => it(`${schema} matches ${show(json)}`, () => {
+    if (json !== validate(E(schema))(json))
       throw new Error("Didn't return same value")
   }))
 
-const mismatches = (schemaString, schema, ...jsons) => jsons.forEach(
-  json => it(`${schemaString} mismatches ${show(json)}`, () => {
+const mismatches = (schema, ...jsons) => jsons.forEach(
+  json => it(`${schema} mismatches ${show(json)}`, () => {
     let raised
     try {
-      validate(schema)(json)
+      validate(E(schema))(json)
     } catch (e) {
       raised = e
     }
@@ -40,57 +49,57 @@ const basicJSON = [null, 0, 1, false, true, "", [], {}]
 const nonJSON = [NaN, Infinity, -Infinity, undefined]
 
 describe("/.../", () => {
-  mismatches("/.*/", /.*/, ...basicJSON.filter(isntType("string")))
+  mismatches("/.*/", ...basicJSON.filter(isntType("string")))
 
-  matches("/^full$/", /^full$/, "full")
-  mismatches("/^full$/", /^full$/, " full ")
+  matches("/^full$/", "full")
+  mismatches("/^full$/", " full ")
 
-  matches("/partial/", /partial/, "partial")
-  matches("/partial/", /partial/, " partial ")
+  matches("/partial/", "partial")
+  matches("/partial/", " partial ")
 })
 
 describe("...", () => {
-  mismatches('""', "", ...basicJSON.filter(isntType("string")))
-  matches('""', "", "")
-  matches('"x"', "x", "x")
+  mismatches('""', ...basicJSON.filter(isntType("string")))
+  matches('""', "")
+  matches('"x"', "x")
 })
 
 describe("<number>", () => {
-  mismatches("0", 0, ...basicJSON.filter(isntType("number")).concat([1]))
-  mismatches("1", 1, ...basicJSON.filter(isntType("number")).concat([0]))
-  matches("1", 1, 1)
-  matches("0", 0, 0)
+  mismatches("0", ...basicJSON.filter(isntType("number")).concat([1]))
+  mismatches("1", ...basicJSON.filter(isntType("number")).concat([0]))
+  matches("1", 1)
+  matches("0", 0)
 })
 
 describe("any", () => {
-  matches("any", any, ...basicJSON)
-  mismatches("any", any, ...nonJSON)
+  matches("any", ...basicJSON)
+  mismatches("any", ...nonJSON)
 })
 
 describe("and", () => {
-  matches("and()", and(), ...basicJSON)
-  mismatches("and(0,1)", and(0,1), 0)
+  matches("and()", ...basicJSON)
+  mismatches("and(0,1)", 0)
 })
 
 describe("number", () => {
-  mismatches("number", number, ...basicJSON.filter(isntType("number")))
+  mismatches("number", ...basicJSON.filter(isntType("number")))
 })
 
 describe("or", () => {
-  mismatches("or()", or(), ...basicJSON)
+  mismatches("or()", ...basicJSON)
 
-  matches("or(1, 2, or(3))", or(1, 2, or(3)), 1, 2, 3)
-  mismatches("or(1, 2, or(3))", or(1, 2, or(3)), 0)
+  matches("or(1, 2, or(3))", 1, 2, 3)
+  mismatches("or(1, 2, or(3))", 0)
 })
 
 describe("{ ... }", () => {
-  mismatches("{}", {}, ...basicJSON.filter(isntType("object")))
-  matches("{}", {}, {})
-  matches("{required: true}", {required: true}, {required: true, extra: 1})
-  mismatches("{required: true}", {required: true}, {extra: 1})
-  matches("{optional: optional(1)}", {optional: optional(1)}, {})
-  matches("{optional: optional(1)}", {optional: optional(1)}, {optional: 1})
-  mismatches("{optional: optional(1)}", {optional: optional(1)}, {optional: optional(2)})
+  mismatches("{}", ...basicJSON.filter(isntType("object")))
+  matches("{}", {})
+  matches("{required: true}", {required: true, extra: 1})
+  mismatches("{required: true}", {extra: 1})
+  matches("{optional: optional(1)}", {})
+  matches("{optional: optional(1)}", {optional: 1})
+  mismatches("{optional: optional(1)}", {optional: optional(2)})
 })
 
 describe("mismatch", () => {
@@ -99,18 +108,12 @@ describe("mismatch", () => {
       or({x: 1}, {y: [1]}),
       assertEq(),
       mismatch =>
-        assertEq('{... "x": undefined ...} and {... "y": [... 0: 2 ...] ...}')
-      (mismatch.toString()))({y: [2]})
+        assertEq('{... "x": undefined ...} and {... "y": [... 0: 2 ...] ...}')(mismatch.toString()))({y: [2]})
   })
 })
 
 describe("lazy", () => {
-  const branch = {lhs: optional(lazy(() => branch)),
-                  key: number,
-                  rhs: optional(lazy(() => branch))}
-  const tree = or(null, branch)
-
-  matches("tree", tree, null)
-  matches("tree", tree, {key: 1})
-  matches("tree", tree, {lhs: {key: 0}, key: 1, rhs: {lhs: {key: 2}, key: 3, rhs: {key: 4}}})
+  matches("tree", null)
+  matches("tree", {key: 1})
+  matches("tree", {lhs: {key: 0}, key: 1, rhs: {lhs: {key: 2}, key: 3, rhs: {key: 4}}})
 })
